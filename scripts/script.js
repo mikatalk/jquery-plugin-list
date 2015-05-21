246b84f0211838724f7ea5cdb6b04a87eb657c3f
Mika <mika.indie@gmail.com>
2015-05-20 23:04:10 -0700

    add page

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+
+$(function(){
+
+  'use strict';
+
+  var data = { tiles:[
+  	// {''}
+  ]};
+
+	$(".list.mixed").list({
+		mode: 'mixed'
+	});
+
+	$(".list.fixed").list({
+		mode: 'fixed'
+	});
+
+
+	$(".list.column").list({
+		mode: 'column'
+	});
+
+});
